2a9dd5f8b91aa6ff2b420277451d99aef9fd8537
Jonathan Kok <jon.825@Jonathans-MacBook-Pro.local>
2017-08-23 18:33:54 -0400

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('index', __dirname + '/index');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
