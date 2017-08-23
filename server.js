9312b6a5e86bb4cd96b73bd32f6c469703a8e315
Jonathan Kok <jon.825@Jonathans-MacBook-Pro.local>
2017-08-23 18:24:24 -0400

    test

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
 
-app.get("/", (req, res)=>{
-  res.send('hello world');
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index/portfolio');
 });
 
-app.listen(port)
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
